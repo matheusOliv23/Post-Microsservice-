59e86e1e9b6472f9e0660c1f28991207d4dd016b
matheusOliv23 <matheushenri1996@gmail.com>
2023-06-11 10:30:46 -0300

    feat: add moderation service

---
+++ moderation/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {});
+
+app.listen(4003, () => {
+  console.log("Listening to 4003");
+});
