7850ead84871f76f2724bb9798da02d4c25c8225
matheusOliv23 <matheushenri1996@gmail.com>
2023-06-11 10:43:17 -0300

    feat: handling moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,29 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.include("orange") ? "rejected" : "approved";
+
+    await axios
+      .post("http://localhost:4003/events", {
+        type: "CommentModerated",
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log("Listening to 4003");
